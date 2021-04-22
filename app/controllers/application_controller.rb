c64c1813410f17fe444f067786f8f4fae18a7ed2
kazukitsuyama <sh.a.ma.nn.13.kk@gmail.com>
2021-04-22 12:54:08 +0900

    ログイン・アウトのページ遷移

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  def after_sign_in_path_for(resource)
+    mains_index_path
+  end
+
+  def after_sign_out_path_for(resource)
+    tops_index_path
+  end
+
+  private
   
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
