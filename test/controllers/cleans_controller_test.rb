c7b7cdc39741e3f409a5fa9d3d3b88593716853e
kazukitsuyama <sh.a.ma.nn.13.kk@gmail.com>
2021-04-24 02:58:51 +0900

    cleanコントローラー作成

---
+++ test/controllers/cleans_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class CleansControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get cleans_index_url
+    assert_response :success
+  end
+
+end
