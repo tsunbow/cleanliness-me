41e6546fefbe66cf96dadea5d5de49bed8e9469d
kazukitsuyama <sh.a.ma.nn.13.kk@gmail.com>
2021-04-25 04:02:21 +0900

    form書き換え

--- app/controllers/cleans_controller.rb
+++ app/controllers/cleans_controller.rb
@@ -7,6 +7,14 @@ class CleansController < ApplicationController
   end
 
   def create
+    @clean = Clean.new(clean_params)
+  end
+
+
+  private
+
+  def clean_params
+    params.require(:clean).permit(:image, :clean_name, :text, :cleaning_place, :status_id, :support_id, :important_id).merge(user_id: current_user.id)
   end
 
 end
