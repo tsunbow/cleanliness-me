560f1d8c4a0e9d346c4e2da7c895b435965daaf8
kazukitsuyama <sh.a.ma.nn.13.kk@gmail.com>
2021-05-02 05:01:14 +0900

    Delete 20210430184716_create_completes.rb

--- db/migrate/20210430184716_create_completes.rb
+++
@@ -1,9 +0,0 @@
-class CreateCompletes < ActiveRecord::Migration[6.0]
-  def change
-    create_table :completes do |t|
-      t.references :user,               null: false, foreign_key: true
-      t.references :clean,               null: false, foreign_key: true
-      t.timestamps
-    end
-  end
-end
